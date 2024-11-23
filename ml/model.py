ca3b04c290b3c4e93db283308f76473facdd2929
Emma Pedersen <epede24@wgu.edu>
2024-11-23 15:28:55 -0600

    Making fixes to model.py and train_model.py

--- ml/model.py
+++ ml/model.py
@@ -2,8 +2,8 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
-from sklearn.model_selection import train_test_split
-from sklearn import tree
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -21,18 +21,24 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    # Split data into training and testing
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.3, random_state=33
-    )
 
     # Initialize the model
-    model = tree.DecisionTreeClassifier()
+    model = LogisticRegression(max_iter=300)
 
-    # Train the model
-    model.fit(X_train, y_train)
+    # Define hyperparameter grid
+    params = {
+        'C': [0.1, 1, 10, 100],
+        'penalty': ['l2']
+    }
 
-    return model
+    # Use GridSearch to tune
+    grid_search = GridSearchCV(model, params, cv=5)
+    
+    grid_search.fit(X_train, y_train)
+
+    # Get and return the best-performing model
+
+    return grid_search.best_estimator_
 
 def compute_model_metrics(y, preds):
     """
@@ -94,6 +100,7 @@ def load_model(path):
     # TODO: implement the function
     with open(path, "rb") as f:
         model = pickle.load(f)
+    return model
 
 
 def performance_on_categorical_slice(
@@ -133,6 +140,10 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+
+    #slice data for input
+    data = data[data[column_name] == slice_value]
+
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
--- train_model.py
+++ train_model.py
@@ -13,14 +13,14 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "/home/emmaep/Deploying-aScalable-ML-Pipeline-with-FastAPI"
+project_path = "/home/emmaep/Deploying-a-Scalable-ML-Pipeline-with-FastAPI"
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path) # your code here
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, random_state=34)# Your code here
+train, test = train_test_split(data, test_size=0.3, random_state=34)
 
 # DO NOT MODIFY
 cat_features = [
@@ -42,6 +42,7 @@ X_train, y_train, encoder, lb = process_data(
     # do not need to pass encoder and lb as input
     train, 
     categorical_features=cat_features,
+    label="salary",
     training=True
     )
 
@@ -57,19 +58,24 @@ X_test, y_test, _, _ = process_data(
 # TODO: use the train_model function to train the model on the training dataset
 model = train_model(X_train, y_train) # your code here
 
+print(f"Model type: {type(model)}") 
+
 # save the model and the encoder
 model_path = os.path.join(project_path, "model", "model.pkl")
 save_model(model, model_path)
+print("Model saved")
 encoder_path = os.path.join(project_path, "model", "encoder.pkl")
 save_model(encoder, encoder_path)
+print("Encoder saved")
 
 # load the model
 model = load_model(
     model_path
 ) 
+print("Model loaded")
 
 # TODO: use the inference function to run the model inferences on the test dataset.
-preds = inference(model, test) # your code here
+preds = inference(model, X_test) # your code here
 
 # Calculate and print the metrics
 p, r, fb = compute_model_metrics(y_test, preds)
@@ -88,6 +94,7 @@ for col in cat_features:
             column_name=col, 
             slice_value=slicevalue,
             categorical_features=cat_features, 
+            label="salary",
             encoder=encoder, 
             lb=lb, 
             model=model
